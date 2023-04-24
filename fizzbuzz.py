5e8c583b3c57e8a44d6735265cad828cfad54804
chaee813 <chaee813@gmail.com>
2023-04-24 21:42:32 +0900

    feat: Done fizzbuzz
    Print fizzbuzz if i is multiple of 15.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,11 +7,13 @@ def do_fizzbuzz(num:int):
     etc : num
     """
     for i in range(1, num+1):
-        if i%3 == 0:
+        if i%15 == 0:
+            print('fizzbuzz')
+        elif i%3 == 0:
             print('fizz')
-        else : print(f'{i}')
-    print('hello')
-    
+        elif i%5 == 0:
+            print('buzz')
+        else : print(f'{i}')    
 
 
 if __name__=='__main__':
